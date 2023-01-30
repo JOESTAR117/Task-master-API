e562531f24cc7b8da113990f0454ac446c99e28c
Jackson Rios <juniorrios1231@gmail.com>
2023-01-30 12:10:18 -0300

    feat: cors

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import express from 'express';
 import connectDatabase from './database/db.js';
 import dotenv from 'dotenv';
 import routes from './routes/index.js';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 
 connectDatabase();
+
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
